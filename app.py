85a7952d4a08cb35338f171d60d612326c468568
Adrianxx1 <triaxis12q@gmail.com>
2022-08-22 13:57:12 -0500

    commit 10

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route('/')
 def index():
@@ -30,3 +31,8 @@ def get_post(post_id):
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
